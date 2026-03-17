309cffb75ed2b260ed1e069cd274085ec69a927a
zhang <2854764801@qq.com>
2026-03-17 23:01:35 +0800

    first commit

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Random;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Object Sendcode(String phone, HttpSession session) {
+        //1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //2.如果不符合，返回错误信息
+            return Result.fail("手机号不符合");
+        }
+        //3.符合，生成验证码
+        String CacheCode = RandomUtil.randomNumbers(6);
+
+        //4.保存验证码到session
+
+        session.setAttribute("CacheCode", CacheCode);
+        //5.发送验证码
+
+        log.info("验证码消息：{}",CacheCode);
+        log.info("session详细id{}",session.getAttribute("CacheCode"));
+        return Result.ok();
+    }
+}
