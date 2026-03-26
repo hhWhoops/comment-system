182f0e2943fea47b96c551532ac2d0ee4c2dc0b1
zhang <2854764801@qq.com>
2026-03-26 19:15:02 +0800

    实现了好友关注的功能，P87

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,36 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.Follow;
+import com.hmdp.entity.ScrollResult;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IFollowService;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.xml.ws.soap.Addressing;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +43,191 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+
+
+    @Autowired
+    private IUserService userService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private IFollowService followService;
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog -> {
+            this.queryBlogUser(blog);
+            this.isBlogLiked(blog);
+        });
+        return Result.ok(records);
+    }
+
+    @Override
+    public Result queryById(Integer id) {
+        Blog blog = getById(id);
+        if (blog == null) {
+            return Result.fail("评价不存在或已被删除");
+        }
+        queryBlogUser(blog);
+        //查询当前blog是否已经点赞
+        isBlogLiked(blog);
+        return Result.ok(blog);
+    }
+
+    private void isBlogLiked(Blog blog) {
+        //1、获取登录用户
+
+        UserDTO user = UserHolder.getUser();
+        if (user == null) {
+            return;
+        }
+        Long userId = user.getId();
+
+        //2.判断当前登录用户是否已经点赞
+        Double score = stringRedisTemplate.opsForZSet().score(RedisConstants.BLOG_LIKED_KEY + blog.getId(), userId.toString());
+        blog.setIsLike(score != null);
+    }
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
+
+
+
+    @Override
+    public Result lickBlog(Long id) {
+        //1、获取登录用户
+        Long userId = UserHolder.getUser().getId();
+
+        //2.判断当前登录用户是否已经点赞
+        Double score = stringRedisTemplate.opsForZSet().score(RedisConstants.BLOG_LIKED_KEY + id, userId.toString());
+        if (score == null) {
+            //3.如果未点赞，可以点赞
+            //3.1数据库点赞数+1
+            boolean isSuccess = update().setSql("liked = liked +1")
+                    .eq("id", id)
+                    .update();
+            //3.2保存用户到Redis的set集合  zadd key value score
+            if (isSuccess) {
+                stringRedisTemplate.opsForZSet().add(RedisConstants.BLOG_LIKED_KEY + id,
+                        userId.toString(), System.currentTimeMillis());
+            }
+        }else {
+            //4.如果已点赞，取消点赞
+            //4.1数据库点赞数-1
+            boolean isSuccess = update().setSql("liked = liked -1")
+                    .eq("id", id)
+                    .update();
+            //4.2把用户从Redis的set集合移除
+            if (isSuccess) {
+                stringRedisTemplate.opsForZSet().remove(RedisConstants.BLOG_LIKED_KEY + id, userId.toString());
+            }
+        }
+
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result queryBlogLikes(Integer id) {
+        String key = RedisConstants.BLOG_LIKED_KEY + id;
+        //zrange key 0 4  查询zset中前5个元素
+        Set<String> top5 = stringRedisTemplate.opsForZSet().range(key, 0, 4);
+        //如果是空的(可能没人点赞)，直接返回一个空集合
+        if (top5 == null || top5.isEmpty()) {
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = top5.stream().map(Long::valueOf).collect(Collectors.toList());
+        //将ids使用`,`拼接，SQL语句查询出来的结果并不是按照我们期望的方式进行排
+        //所以我们需要用order by field来指定排序方式，期望的排序方式就是按照查询出来的id进行排序
+        String idsStr = StrUtil.join(",", ids);
+        //select * from tb_user where id in (ids[0], ids[1] ...) order by field(id, ids[0], ids[1] ...)
+        List<UserDTO> userDTOS = userService.query().in("id", ids)
+                .last("order by field(id," + idsStr + ")")
+                .list().stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(userDTOS);
+    }
+
+    @Override
+    public Result saveBlog(Blog blog) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        // 保存探店博文
+        save(blog);
+        // 条件构造器
+        LambdaQueryWrapper<Follow> queryWrapper = new LambdaQueryWrapper<>();
+        // 从follow表最中，查找当前用户的粉丝  select * from follow where follow_user_id = user_id
+        queryWrapper.eq(Follow::getFollowUserId, user.getId());
+        //获取当前用户的粉丝
+        List<Follow> follows = followService.list(queryWrapper);
+        for (Follow follow : follows) {
+            Long userId = follow.getUserId();
+            String key = RedisConstants.FEED_KEY + userId;
+            //推送数据
+            stringRedisTemplate.opsForZSet().add(key, blog.getId().toString(), System.currentTimeMillis());
+        }
+        // 返回id
+        return Result.ok(blog.getId());
+    }
+
+    @Override
+    public Result queryBlogOfFollow(Long max, Integer offset) {
+        //1. 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        //2. 查询该用户收件箱（之前我们存的key是固定前缀 + 粉丝id），所以根据当前用户id就可以查询是否有关注的人发了笔记
+        String key = RedisConstants.FEED_KEY + userId;
+        Set<ZSetOperations.TypedTuple<String>> typeTuples = stringRedisTemplate.opsForZSet()
+                .reverseRangeByScoreWithScores(key, 0, max, offset, 2);
+        //3. 非空判断
+        if (typeTuples == null || typeTuples.isEmpty()){
+            return Result.ok(Collections.emptyList());
+        }
+        //4. 解析数据，blogId、minTime（时间戳）、offset，这里指定创建的list大小，可以略微提高效率，因为我们知道这个list就得是这么大
+        ArrayList<Long> ids = new ArrayList<>(typeTuples.size());
+        long minTime = 0;
+        int os = 1;
+        for (ZSetOperations.TypedTuple<String> typeTuple : typeTuples) {
+            //4.1 获取id
+            String id = typeTuple.getValue();
+            ids.add(Long.valueOf(id));
+            //4.2 获取score（时间戳）
+            long time = typeTuple.getScore().longValue();
+            if (time == minTime){
+                os++;
+            }else {
+                minTime = time;
+                os = 1;
+            }
+        }
+        //解决SQL的in不能排序问题，手动指定排序为传入的ids
+        String idsStr = StrUtil.join(",");
+        //5. 根据id查询blog
+        List<Blog> blogs = query().in("id", ids).last("ORDER BY FIELD(id," + idsStr + ")").list();
+        for (Blog blog : blogs) {
+            //5.1 查询发布该blog的用户信息
+            queryBlogUser(blog);
+            //5.2 查询当前用户是否给该blog点过赞
+            isBlogLiked(blog);
+        }
+        //6. 封装结果并返回
+        ScrollResult scrollResult = new ScrollResult();
+        scrollResult.setList(blogs);
+        scrollResult.setOffset(os);
+        scrollResult.setMinTime(minTime);
+        return Result.ok(scrollResult);
+    }
 }
